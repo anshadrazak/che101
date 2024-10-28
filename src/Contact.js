c9d369609c3a0cead762e97e03a03a921aee15ac
anshadrazak <anshadrazakk@gmail.com>
2024-10-28 18:57:29 +0530

    codes

--- src/Contact.js
+++ src/Contact.js
@@ -5,26 +5,25 @@ import './Contact.css'
 
 const Contact = () => {
   return (
-    <div className='contact'><Header/>
-    <div className='ctbody'>
-      <div className='fs'>
+    <div className='contact'>
+      <Header/>
+      <div className='ctbody'>
+        <div className='fs'>
 
-        <h3 className='cttxt'>In Case Of Emergency, <br></br> Just Contact the Heroes.</h3>
+          <h3 className='cttxt'>In Case Of Emergency, <br></br> Just Contact the Heroes.</h3>
+        </div>
       </div>
 
-      
-    </div>
-
-<div className='cntdiv3'>
-                    <div className='onectr'>
-                    <h3 className='hdings'>Emergency Telephone Number: 112</h3>
-                    <p className='defs'>Fire Control Room: 101 (available 24/7)</p>
-                    <p className='defs'>National Disaster Response Force (NDRF): 108 (for Disaster Management Services)</p>
-                    <p className='defs'>Disaster Management Services: 108</p>
-                    </div>
-                    
-                </div>
-      
+      <div className='cntdiv3'>
+        <div className='onectr'>
+          <h3 className='hdings'>Emergency Telephone Number: 112</h3>
+          <p className='defs'>Fire Control Room: 101 (available 24/7)</p>
+          <p className='defs'>National Disaster Response Force (NDRF): 108 (for Disaster Management Services)</p>
+          <p className='defs'>Disaster Management Services: 108</p>
+        </div>
+        <a href='tel:+91101'><button className='reportbtn'>Report A Fire</button></a>
+                        
+      </div>
     </div>
   )
 }
